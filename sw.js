3e9a3499b46c6e1a7090414ac9d320c9bb58bbdb
NtoStudy <2943343011@qq.com>
2024-08-22 15:13:19 +0800

    Site updated: 2024-08-22 15:13:12

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1724310498871";
+const VERSION = "1724310787330";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
